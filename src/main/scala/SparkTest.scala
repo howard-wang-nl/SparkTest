074fba8dec7e48a7a2e69af233aa0ae5d41a9889
Vijay Kiran <mail@vijaykiran.com>
2015-10-31 07:16:07 +0100

    Use kryo to serialize Array

--- src/main/scala/SparkTest.scala
+++ src/main/scala/SparkTest.scala
@@ -3,6 +3,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SimpleApp {
   def main2(args: Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application")
+    conf.registerKryoClasses(Array(classOf[(LongWritable, Text)]))
     val sc = new SparkContext(conf)
     val inputPath = "/Users/hwang/IdeaProjects/githubstat/data/"
 
